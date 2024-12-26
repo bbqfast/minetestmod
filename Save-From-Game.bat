d3d2e2536544be80ab73aa3c5fd1942741309472
bbqfast <bbqfast@hotmail.com>
2024-12-25 18:36:52 -0600

    save2

--- Save-From-Game.bat
+++ Save-From-Game.bat
@@ -1,3 +1,3 @@
 
 xcopy C:\Minetest\v5.6\mods\basic_snacks\ basic_snacks\  /s /e /y
-xcopy C:\Minetest\v5.6\mods\maidroid_ng\ maidroid_ng /s /e /y
+xcopy C:\Minetest\v5.6\mods\maidroid_ng\ maidroid_ng\ /s /e /y
